112914a08f334958b725e368123860c67fc288b7
Bogdan <74682561+inspiq@users.noreply.github.com>
2022-09-13 16:59:46 +0400

    Update useHttp.tsx

--- hooks/useHttp.tsx
+++ hooks/useHttp.tsx
@@ -1,7 +1,7 @@
 export const useHttp = () => {
   const request = async(url: string, method: string = 'GET', body: any) => {
     if (method == "POST") {
-      const response = await fetch('http://localhost:3000/api/addOperator', {
+      const response = await fetch('https://payment-terminal-mu.vercel.app/api/addOperator', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json; charset=utf-8;'
@@ -9,7 +9,7 @@ export const useHttp = () => {
         body: JSON.stringify(body)
       });
     } else {
-      const res = await fetch(`http://localhost:3000/${url}`);
+      const res = await fetch(`https://payment-terminal-mu.vercel.app/${url}`);
       const data = await res.json();
       return data
     }
