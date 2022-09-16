cdbf9fb972c06de51a4e22bcf743ae067eaed661
Bogdan <74682561+inspiq@users.noreply.github.com>
2022-09-16 23:52:47 +0400

    Update addOperator.ts

--- pages/api/addOperator.ts
+++ pages/api/addOperator.ts
@@ -1,6 +1,6 @@
 const fs = require('fs'); 
 import type { NextApiRequest, NextApiResponse } from 'next';
-import operators from '../../public/operators.json';
+import operators from './data/J_n5Fz-ImaCg1gCf33PMO/index.json';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     let operator = {
@@ -15,6 +15,6 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 }
 
 const updateData = () => {
-  fs.writeFileSync('./public/operators.json', JSON.stringify(operators, null, 2)); 
+  fs.writeFileSync('./data/J_n5Fz-ImaCg1gCf33PMO/index.json', JSON.stringify(operators, null, 2)); 
 }
 
