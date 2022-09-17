cbc690a82b33df240044f507135c80c850f91cee
Bogdan <74682561+inspiq@users.noreply.github.com>
2022-09-17 19:23:18 +0400

    Update useHttp.tsx

--- hooks/useHttp.tsx
+++ hooks/useHttp.tsx
@@ -3,7 +3,7 @@ type Nullable<T> = T | null;
 export const useHttp = () => {
   const request = async(url: string, method: string = 'GET', body: string | {} | Nullable<number>) => {
     if (method == "POST") {
-      const response = await fetch('https://gleaming-horse-8f4804.netlify.app/api/addOperator', {
+      const response = await fetch('https://payment-terminal-mu.vercel.app/api/addOperator', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json; charset=utf-8'
@@ -11,7 +11,7 @@ export const useHttp = () => {
         body: JSON.stringify(body)
       });
     } else {
-      const res = await fetch(`https://gleaming-horse-8f4804.netlify.app/${url}`);
+      const res = await fetch(`https://payment-terminal-mu.vercel.app/${url}`);
       const data = await res.json();
       return data
     }
